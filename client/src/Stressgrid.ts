9256a82801e04c3679e542bf32fa11aa49912b50
KT315 <kt315@ml1.net>
2019-05-31 20:22:08 -0700

    Improve CLI: CSV URL in stdout and info in stderr

---
+++ client/src/Stressgrid.ts
@@ -0,0 +1,172 @@
+import _ from 'lodash';
+import ReconnectingWebSocket from 'reconnecting-websocket';
+import WS from 'ws';
+
+export interface IScriptError {
+  description: string;
+  line: number;
+}
+
+export interface ITelemetry {
+  cpu: number[];
+  network_rx: number[];
+  network_tx: number[];
+  active_count: number[];
+  last_errors?: Map<string, number[]>;
+  last_script_error?: IScriptError;
+  generator_count: number[];
+}
+
+export interface IRun {
+  id: string;
+  name: string;
+  state: string;
+  remaining_ms: number;
+}
+
+export interface IResult {
+  csv_url?: string;
+  cw_url?: string;
+}
+
+export interface IReport {
+  id: string;
+  name: string;
+  errors?: Map<string, number[]>;
+  script_error?: IScriptError;
+  max_cpu: number;
+  max_network_rx: number;
+  max_network_tx: number;
+  max_generator_count: number;
+  result: IResult;
+}
+
+export interface IInit {
+  reports: IReport[];
+  grid: IGrid;
+}
+
+export interface IGrid {
+  telemetry: ITelemetry;
+  run: IRun | null;
+}
+
+export interface INotify {
+  grid_changed?: IGrid;
+  report_added?: IReport;
+  report_removed?: { id: string };
+}
+
+export interface IBlock {
+  script?: string;
+  params?: object;
+  size?: number;
+}
+
+export interface IAddress {
+  host: string;
+  port?: number;
+  protocol?: string;
+}
+
+export interface IOpts {
+  ramp_steps?: number;
+  rampup_step_ms?: number;
+  sustain_ms?: number;
+  rampdown_step_ms?: number;
+}
+
+export interface IRunPlan {
+  name: string;
+  blocks: IBlock[];
+  addresses: IAddress[];
+  opts: IOpts;
+  script?: string;
+}
+
+export interface IRemoveReport {
+  id: string;
+}
+
+export interface IMessage {
+  init?: IInit;
+  notify?: INotify;
+  run_plan?: IRunPlan;
+  remove_report?: IRemoveReport;
+}
+
+export interface IStressgridEvents {
+  init(grid: IGrid, reports: IReport[]): void;
+  updateGrid(grid: IGrid): void;
+  addReport(report: IReport): void;
+  deleteReport(id: string): void;
+  disconnected(): void;
+}
+
+export class Stressgrid {
+  private ws: ReconnectingWebSocket;
+  private events: IStressgridEvents;
+
+  constructor(events: IStressgridEvents) {
+    this.events = events;
+  }
+
+  public connect(wsUrl: string) {
+    this.ws = new ReconnectingWebSocket(wsUrl, [], {
+      WebSocket: WS
+    });
+    this.ws.onmessage = (e) => {
+      _.each(JSON.parse(e.data), (message: IMessage) => {
+        this.handle(message);
+      });
+    }
+    this.ws.onclose = (e) => {
+      this.events.disconnected();
+    }
+  }
+
+  public disconnect() {
+    this.ws.close();
+    this.ws.onmessage = undefined;
+  }
+
+  public run(runPlan: IRunPlan) {
+    this.send([{
+      run_plan: runPlan
+    }]);
+  }
+
+  public abortRun() {
+    this.send(['abort_run']);
+  }
+
+  public removeReport(id: string) {
+    this.send([{
+      remove_report: {
+        id
+      }
+    }]);
+  }
+
+  private send(messages: Array<IMessage | string>) {
+    this.ws.send(JSON.stringify(messages));
+  }
+
+  private handle(message: IMessage) {
+    const { init, notify } = message;
+    if (init) {
+      this.events.init(init.grid, init.reports);
+    }
+    if (notify) {
+      if (notify.grid_changed) {
+        this.events.updateGrid(notify.grid_changed);
+      }
+      if (notify.report_added) {
+        this.events.addReport(notify.report_added);
+      }
+      if (notify.report_removed) {
+        this.events.deleteReport(notify.report_removed.id);
+      }
+    }
+  }
+}
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,173 +1,12 @@
 #! /usr/bin/env node
 
-import * as _ from 'lodash';
-
-import * as program from 'commander';
-import ReconnectingWebSocket from 'reconnecting-websocket';
-import * as WS from 'ws';
-import * as fs from 'fs';
-
-interface IScriptError {
-  description: string;
-  line: number;
-}
-
-interface ITelemetry {
-  cpu: number[];
-  network_rx: number[];
-  network_tx: number[];
-  active_count: number[];
-  last_errors?: Map<string, number[]>;
-  last_script_error?: IScriptError;
-  generator_count: number[];
-}
-
-interface IRun {
-  id: string;
-  name: string;
-  state: string;
-  remaining_ms: number;
-}
-
-interface IResult {
-  csv_url?: string;
-  cw_url?: string;
-}
-
-interface IReport {
-  id: string;
-  name: string;
-  errors?: Map<string, number[]>;
-  script_error?: IScriptError;
-  max_cpu: number;
-  max_network_rx: number;
-  max_network_tx: number;
-  max_generator_count: number;
-  result: IResult;
-}
-
-interface IInit {
-  reports: IReport[];
-  grid: IGrid;
-}
-
-interface IGrid {
-  telemetry: ITelemetry;
-  run: IRun | null;
-}
-
-interface INotify {
-  grid_changed?: IGrid;
-  report_added?: IReport;
-  report_removed?: { id: string };
-}
-
-interface IBlock {
-  script?: string;
-  params?: object;
-  size?: number;
-}
-
-interface IAddress {
-  host: string;
-  port?: number;
-  protocol?: string;
-}
-
-interface IOpts {
-  ramp_steps?: number;
-  rampup_step_ms?: number;
-  sustain_ms?: number;
-  rampdown_step_ms?: number;
-}
-
-interface IRunPlan {
-  name: string;
-  blocks: IBlock[];
-  addresses: IAddress[];
-  opts: IOpts;
-  script?: string;
-}
-
-interface IRemoveReport {
-  id: string;
-}
-
-interface IMessage {
-  init?: IInit;
-  notify?: INotify;
-  run_plan?: IRunPlan;
-  remove_report?: IRemoveReport;
-}
-
-interface IStressgridEvents {
-  init(grid: IGrid, reports: IReport[]): void;
-  updateGrid(grid: IGrid): void;
-}
-
-export class Stressgrid {
-  private ws: ReconnectingWebSocket;
-  private events: IStressgridEvents;
-
-  constructor(events: IStressgridEvents) {
-    this.events = events;
-  }
-
-  public connect(wsUrl: string) {
-    this.ws = new ReconnectingWebSocket(wsUrl, [], {
-      WebSocket: WS
-    });
-    this.ws.onmessage = (e) => {
-      _.each(JSON.parse(e.data), (message: IMessage) => {
-        this.handle(message);
-      });
-    }
-  }
-
-  public disconnect() {
-    this.ws.close();
-  }
-
-  public run(runPlan: IRunPlan) {
-    this.send([{
-      run_plan: runPlan
-    }]);
-  }
-
-  public abortRun() {
-    this.send(["abort_run"]);
-  }
-
-  public removeReport(id: string) {
-    this.send([{
-      remove_report: {
-        id
-      }
-    }]);
-  }
-
-  private send(messages: Array<IMessage | string>) {
-    this.ws.send(JSON.stringify(messages));
-  }
-
-  private handle(message: IMessage) {
-    const { init, notify } = message;
-    if (init) {
-      this.events.init(init.grid, init.reports);
-    }
-    if (notify) {
-      if (notify.grid_changed) {
-        this.events.updateGrid(notify.grid_changed);
-      }
-      if (notify.report_added) {
-        //this.events.addReport(notify.report_added);
-      }
-      if (notify.report_removed) {
-        //this.events.deleteReport(notify.report_removed.id);
-      }
-    }
-  }
-}
+import _ from 'lodash';
+import program from 'commander';
+import fs from 'fs';
+import log from 'single-line-log';
+import chalk from 'chalk';
+import filesize from 'filesize';
+import { Stressgrid, IRunPlan } from './Stressgrid'
 
 program
   .option('-c, --coordinator-host <string>', 'coordinator host (localhost)')
@@ -185,57 +24,113 @@ program
   .option('--sustain <number>', 'sustain seconds (900)', parseInt)
   .option('--rampdown <number>', 'rampdown seconds (900)', parseInt)
   .action((name, script, options) => {
+    const coordinatorHost = _.defaultTo(program.coordinatorHost, 'localhost');
     if (!fs.existsSync(script)) {
       console.error('script file must exist');
-      return;
+      process.exit(-1);
     }
-    console.log('connecting...')
-    const sg = new Stressgrid({
-      init: (grid, reports) => {
-        if (!grid.telemetry.generator_count[0]) {
-          console.error('must have at least one generator');
-          sg.disconnect();
-          return;
-        }
-        if (grid.run) {
-          console.error('already running, please stop current run');
-          sg.disconnect();
-          return;
-        }
-        console.log('running...')
-        sg.run({
-          addresses: _.map(_.split(_.defaultTo(options.targetHosts, 'localhost'), ','), host => {
-            return {
-              host: _.trim(host),
-              port: _.defaultTo(options.targetPort, 5000),
-              protocol: _.defaultTo(options.targetProtocol, 'http')
-            };
-          }),
-          blocks: [{
-            params: JSON.parse(_.defaultTo(options.scriptParams, '{}')),
-            size: _.defaultTo(options.size, 10000)
-          }],
-          name,
-          opts: {
-            ramp_steps: 1000,
-            rampdown_step_ms: _.defaultTo(options.rampdown, 900),
-            rampup_step_ms: _.defaultTo(options.rampup, 900),
-            sustain_ms: _.defaultTo(options.sustain, 900) * 1000
-          },
-          script: fs.readFileSync(script).toString()
-        });
+    const plan: IRunPlan = {
+      addresses: _.map(_.split(_.defaultTo(options.targetHosts, 'localhost'), ','), host => {
+        return {
+          host: _.trim(host),
+          port: _.defaultTo(options.targetPort, 5000),
+          protocol: _.defaultTo(options.targetProtocol, 'http')
+        };
+      }),
+      blocks: [{
+        params: JSON.parse(_.defaultTo(options.scriptParams, '{}')),
+        size: _.defaultTo(options.size, 10000)
+      }],
+      name,
+      opts: {
+        ramp_steps: 1000,
+        rampdown_step_ms: _.defaultTo(options.rampdown, 900),
+        rampup_step_ms: _.defaultTo(options.rampup, 900),
+        sustain_ms: _.defaultTo(options.sustain, 900) * 1000
       },
-      updateGrid: (grid) => {
-        if (!grid.run || grid.run.name !== name) {
-          sg.disconnect();
-        }
-      }
-    });
-    sg.connect('ws://' + _.defaultTo(program.coordinatorHost, 'localhost') + ':8000/ws');
+      script: fs.readFileSync(script).toString()
+    };
+    runPlan(coordinatorHost, plan);
   });
 
 program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
+}
+
+function runPlan(coordinatorHost: string, plan: IRunPlan) {
+  let result = 0;
+  let id: string | null = null;
+  const sg = new Stressgrid({
+    init: (grid, reports) => {
+      if (!grid.telemetry.generator_count[0]) {
+        console.error(chalk.red('ERROR:  must have at least one generator'));
+        sg.disconnect();
+        result = -1;
+        return;
+      }
+      if (grid.run) {
+        console.error(chalk.red('ERROR:  already running, please stop current run'));
+        sg.disconnect();
+        result = -1;
+        return;
+      }
+      sg.run(plan);
+    },
+    updateGrid: (grid) => {
+      if (grid.run) {
+        if (id === null) {
+          id = grid.run.id;
+        }
+        else if (id === grid.run.id) {
+          if (grid.telemetry.last_script_error) {
+            log.stderr.clear();
+            console.error(chalk.red('ERROR:  ' + chalk.bold('line ' + grid.telemetry.last_script_error.line + ': ' + grid.telemetry.last_script_error.description)));
+            sg.abortRun();
+          }
+          else {
+            let errors = '';
+            if (grid.telemetry.last_errors) {
+              errors = chalk.red(_.join(_.map(_.toPairs(grid.telemetry.last_errors), pair => {
+                const recentCounts = pair[1];
+                const type = pair[0];
+                return 'ERROR:  ' + chalk.bold(type) + ' occurred ' + chalk.bold(_.defaultTo(recentCounts[0], 0)) + ' times\r\n';
+              }), ''));
+            }
+
+            log.stderr(chalk.green(
+              'RUN ID: ' + chalk.bold(id) + '\n\r' +
+              'STATE:  ' + chalk.bold(grid.run.state + ' ' + Math.trunc(grid.run.remaining_ms / 1000).toString() + ' seconds') + ' remaining\r\n' +
+              'ACTIVE: ' + chalk.bold(_.defaultTo(grid.telemetry.active_count[0], 0).toString() + ' devices') + '\r\n' +
+              'CPU:    ' + chalk.bold(Math.trunc(_.defaultTo(grid.telemetry.cpu[0], 0) * 100).toString() + '%') + '\r\n' +
+              'NET RX: ' + chalk.bold(filesize(_.defaultTo(grid.telemetry.network_rx[0], 0)) + '/sec') + '\r\n' +
+              'NET TX: ' + chalk.bold(filesize(_.defaultTo(grid.telemetry.network_tx[0], 0)) + '/sec') + '\r\n' +
+              errors));
+          }
+        }
+      }
+    },
+    addReport: (report) => {
+      if (report.id === id) {
+        console.log('http://' + coordinatorHost + ':8000/' + report.result.csv_url);
+        sg.disconnect();
+        if (report.script_error || report.errors) {
+          result = -1;
+        }
+      }
+    },
+    deleteReport: (id: string) => { },
+    disconnected: () => {
+      process.exit(result);
+    }
+  });
+  sg.connect('ws://' + coordinatorHost + ':8000/ws');
+  process.on('SIGINT', function () {
+    log.stderr.clear();
+    console.error(chalk.red('ABORTED'));
+    sg.abortRun();
+    sg.disconnect();
+    result = -1;
+  });
 }
