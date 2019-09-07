fcc3971c578a5f9f888a1c04bc68c61952512dfc
KT315 <kt315@ml1.net>
2019-09-06 19:47:00 -0700

    Parametrize GCP project ID

--- etc/packer.sh
+++ etc/packer.sh
@@ -9,7 +9,7 @@ tar -zcvf /tmp/$RELEASE/$GIT_HASH/release.tar.gz .
 popd
 
 pushd packer/
-packer build -var git_hash=$GIT_HASH -var release=$RELEASE packer.json
+packer build -var git_hash=$GIT_HASH -var release=$RELEASE $@ packer.json
 popd
 
 rm -rf /tmp/$RELEASE/$GIT_HASH
